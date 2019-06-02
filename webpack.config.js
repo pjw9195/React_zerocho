c60929b33c25587396a4e9fabad444ebc9ab4ef1
Park <pjw9195@naver.com>
2019-06-02 21:32:37 +0900

    Webpack 구성

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const path = require('path');
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', //실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  entry: {
+    app: ['./client'],
+  }, //입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/, //jsx와 js를 룰을적용 적용하겟다는 뜻
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-class-properties'],
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'), //C;\users\jinwoo\desktop\zerocho
+    filename: 'app.js',
+  }, //출력
+};
